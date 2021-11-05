db0429d3ed0fb3895f247567796b92e68f13c6bc
Muhammad Tanvir Bin Ali <ali2912@outlook.com>
2021-11-05 09:54:47 +0000

    prettier

--- src/client/Form.js
+++ src/client/Form.js
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // New Post form
 const Form = ({ onAdd }) => {
   // Store the input in state variables when the user types
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   return (
     <form>
       <input
-        placeholder={'Title'}
+        placeholder={"Title"}
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
       <textarea
-        placeholder={'Content'}
+        placeholder={"Content"}
         value={content}
         onChange={(e) => setContent(e.target.value)}
       />
 
       {/* When the user clicks the button, call the onAdd prop */}
-      <button onClick={() => onAdd({ title, content })}>Post</button>
+      <button
+        onClick={() => {
+          onAdd({ title, content });
+        }}
+      >
+        Post
+      </button>
       <hr />
     </form>
   );
